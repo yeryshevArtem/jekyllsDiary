7cd4424efe54654c65d196b6ee2b1efc82aaf0a8
zippi-wzp <truebatmandc@gmail.com>
2015-07-27 12:10:39 -0700

    We created the diary of events and spended time

--- main.js
+++ main.js
@@ -4,47 +4,37 @@ var clear = function (event) {
     $('label').remove();
 };
 
+
 var application = function () {
     var buttonClicked = function (event) {
         var name = $("input.name").val();
         if ("jekyll" === name.toLowerCase()) {
             alert("Thank you for authorization!");
             clear(event);
-        } else {
+
+
+           $("body").append('<p><label>What did you do today?</label></p>');
+           $("body").append('<textarea class="myEvents"></textarea>');
+           $("body").append('<p><input class="numberOfHours" type="text">How much times I spent today?</p>');
+           $("body").append('<button  class="submit">Enter</button>');
+
+           $("button").click(function() {
+             $('.numberOfHours').val('');
+             $('.myEvents').val('');
+            alert("Activity has beem logged!");
+
+          });
+
+
+             }
+
+       else {
             alert("Wrong name!");
         }
     };
     $("button.submit").click(buttonClicked);
+
 };
 
+
 $(document).ready(application);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
