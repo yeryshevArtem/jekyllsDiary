e19e30775f97dd53bc05f304163fe438a03953a3
Zippi-wzp <truebatmandc@gmail.com>
2015-08-20 20:56:48 -0400

    Fixed get and post activities

--- index.php
+++ index.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Diary</title>
-        <script type="text/javascript" src="jquery.min.js"></script>
-        <script type="text/javascript" src="bootstrap.js"></script>
-        <script type="text/javascript" src="main.js"></script>
-        <link href="bootstrap.css" rel="stylesheet">
-    </head>
-    <body>
-    <div class="modal fade" role="dialog" id="myModal">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">Edit activity</h4>
+<head>
+    <meta charset="utf-8">
+    <title>Diary</title>
+    <script type="text/javascript" src="jquery.min.js"></script>
+    <script type="text/javascript" src="bootstrap.js"></script>
+    <script type="text/javascript" src="main.js"></script>
+    <link href="bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<div class="modal fade" role="dialog" id="myModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Edit activity</h4>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label>Description</label>
+                    <input type="text" class="form-control activityDescription" placeholder="Description">
                 </div>
-                <div class="modal-body">
-                    <div class="form-group">
-                        <label>Description</label>
-                        <input type="text" class="form-control description" placeholder="Description">
-                    </div>
-                    <div class="form-group">
-                        <label>Time spent</label>
-                        <input type="text" class="form-control timespent" placeholder="Hours">
-                    </div>
+                <div class="form-group">
+                    <label>Time spent</label>
+                    <input type="text" class="form-control spentTime" placeholder="Hours">
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary submitEdit">Save changes</button>
             </div>
         </div>
     </div>
-    </body>
+</div>
+</body>
 </html>
--- main.js
+++ main.js
@@ -1,45 +1,60 @@
 var application = function () {
     var body = $("body");
 
-
-
-
     createActivityForm(body);
     displayActivitiesTable();
 
     getActivities().then(function () {
-         //$("button.edit").click
-         //   (function(event) {
-         //       $(event.modal).modal('show');
-         //   });
-        $("button.delete").click
-            (function (event) {
-                $(event.target).parent().parent().remove();
-                var requestParamsDelete = {
-                    url: 'http://127.0.0.1:8000/activities',
-                    method: "DELETE",
-                    success: function (response) {
-                        alert("Activity has been deleted!");
-                    },
-                    fail: function (response) {
-                        console.log(response);
-                        alert("Bad request!");
-                    }
-                };
+        $("button.edit").click(function(event) {
+            var currentRow = $(event.target).parent().parent();
+            $('input.activityDescription').val($(currentRow.find("td")[0]).text());
+            $('input.spentTime').val($(currentRow.find("td")[1]).text());
+        });
+
+        $("button.delete").click(function (event) {
+            console.log("BBBBBBBBBBBBBBB");
+            $.ajax({
+                url: 'http://127.0.0.1:8000/activities',
+                method: "POST",
+                data: JSON.stringify({ activity: { method: "DELETE" }}),
+               success: function (response) {
+                    $(event.target).parent().parent().remove();
+                    alert("Activity has been deleted!");
+                },
+                fail: function (response) {
+                    console.log(response);
+                    alert("Bad request!");
+                }
             });
+        });
+
+        $("button.submitEdit").click(function (event) {
+            var activity = $(".activityDescription").val();
+            var timeSpent = $(".spentTime").val();
+
+            var requestData = JSON.stringify({ activity: { description: activity, hours: timeSpent }});
+
+            $.ajax({
+                url: 'http://127.0.0.1:8000/activities',
+                method: "PUT",
+                data: requestData,
+                success: function (response) {
+                    $(event.target).closest('#myModal').modal("hide");
+                    alert("Activity has been successfully updated!");
+                },
+                fail: function (response) {
+                    console.log(response);
+                    alert("Bad request!");
+                }
+            });
+        });
     });
-
-    var placeholderAct = $("body>table>thead>tr").find("th.activity").text();
-    var placeholderTime = $("body>table>thead>tr").find("th.hoursSpent").text();
-
-    $('input.description').attr('placeholder', placeholderAct);
-    $('input.timespent').attr('placeholder', placeholderTime);
 
     $("a.submitNote").click(function() {
         var activity = $(".activity").val();
         var timeSpent = $(".timeSpent").val();
 
-        var requestData = JSON.stringify({ activity: { description: activity, timeSpent: timeSpent }});
+        var requestData = JSON.stringify({ activity: { description: activity, hours: timeSpent }});
 
         var requestParams = {
             url: 'http://127.0.0.1:8000/activities',
@@ -62,21 +77,21 @@ var application = function () {
 
 $(document).ready(application);
 
-
-
-
-
-//ВСЕ ЭТО УЖЕ ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ. НУ ТАМ ВСЯКИЕ ТАМ ОБРАБОТЧИКИ НУ ТАМ ЗАПРОСЫ ПОСЫЛАЮТ, НУ ТАМ
-//В DOM ЧТО-ТО ДОБАВЛЯЮТ
-
-
-
 var createActivityForm = function (body) {
-    // Здеся просто показываем модальное окно которое уже есть у нас в  DOM'е
-};
-
-var createEditActivityForm = function (body, timeSpent) {
-    // Здеся просто показываем модальное окно которое уже есть у нас в  DOM'е
+    console.log("Wake up, Neo!");
+    body.append(
+        '<form>' +
+        '<div class="form-group">' +
+        '<label>Enter activity:</label>' +
+        '<textarea class="activity form-control"></textarea>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label>Time spent:</label>' +
+        '<input class="timeSpent form-control" type="text">' +
+        '</div>' +
+        '<a href="#" class="submitNote btn btn-default">Submit</a>' +
+        '</form>'
+    );
 };
 
 var clearActivityFields = function (bodyTag) {
@@ -87,8 +102,8 @@ var clearActivityFields = function (bodyTag) {
 var displayActivity = function (someActivity, spentTime) {
     $('table.activities tbody').append(
         "<tr>" +
-            "<td>" + someActivity + "</td>" +
-            "<td>" + spentTime + "</td>" +
+        "<td>" + someActivity + "</td>" +
+        "<td>" + spentTime + "</td>" +
         "</tr>"
     );
 };
@@ -96,14 +111,15 @@ var displayActivity = function (someActivity, spentTime) {
 var displayActivitiesTable = function () {
     $("body").append(
         '<table class="table activities table-striped">' +
-            '<thead>' +
-                '<tr>' +
-                    '<th class="activity">Activity</th>' +
-                    '<th class="hoursSpent">Hours spent</th>' +
-                '</tr>' +
-            '</thead>' +
-            '<tbody>' +
-            '</tbody>' +
+        '<thead>' +
+        '<tr>' +
+        '<th class="id">ID</th>' +
+        '<th class="activity">Activity</th>' +
+        '<th class="hoursSpent">Hours spent</th>' +
+        '</tr>' +
+        '</thead>' +
+        '<tbody>' +
+        '</tbody>' +
         '</table>'
     );
 };
@@ -111,10 +127,11 @@ var displayActivitiesTable = function () {
 var appendActivity = function (activity) {
     $("body > table > tbody").append(
         "<tr>" +
-            "<td>" + activity.description + "</td>" +
-            "<td>" + activity.timeSpent + "</td>" +
-            "<td><button class='edit btn btn-primary' data-toggle='modal' data-target='#myModal'>Edit</button></td>" +
-            "<td><button class='delete btn btn-default'>Delete</button></td>" +
+        "<td>" + activity.id + "</td>" +
+        "<td>" + activity.description + "</td>" +
+        "<td>" + activity.hours + "</td>" +
+        "<td><button class='edit btn btn-primary' data-toggle='modal' data-target='#myModal'>Edit</button></td>" +
+        "<td><button class='delete btn btn-default'>Delete</button></td>" +
         "</tr>"
     );
 };
@@ -135,4 +152,3 @@ var getActivities = function () {
         }
     });
 };
-
